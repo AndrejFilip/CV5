90a3811ce79393d8fe8cf223332893fe2c7c2566
AndrejFilip <smejkoTR@gmail.com>
2016-10-20 23:30:51 +0200

    VYtvorenie projektu a suborov

---
+++ src/vrs_cv5.c
@@ -0,0 +1,8 @@
+/*
+ * vrs_cv5.c
+ *
+ *  Created on: Oct 20, 2016
+ *      Author: Andrej
+ */
+
+
---
+++ src/vrs_cv5.h
@@ -0,0 +1,13 @@
+/*
+ * vrs_cv5.h
+ *
+ *  Created on: Oct 20, 2016
+ *      Author: Andrej
+ */
+
+#ifndef VRS_CV5_H_
+#define VRS_CV5_H_
+
+
+
+#endif /* VRS_CV5_H_ */
